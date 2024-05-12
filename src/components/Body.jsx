d8b24ebf49ba00a80721746b87590a602dddacd4
Harshitgupta1304 <guptaharshit1304@gmail.com>
2024-05-13 00:19:09 +0530

    fetch data from Api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,19 @@
 import RestaurantCard from "./RestaurantCard";
 import Data from './../utils/data.json'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Body =() =>{
-  const [topRestaurants, setTopRestaurants] = useState(Data?.data?.restaurants)
-  console.log(topRestaurants)
+  const [topRestaurants, setTopRestaurants] = useState([])
+  console.log(topRestaurants);
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async ()=>{
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    const jsonData = await data.json();
+    console.log(jsonData?.data?.cards[4].card.card.gridElements?.infoWithStyle?.restaurants)
+    setTopRestaurants(jsonData?.data?.cards[4].card.card.gridElements?.infoWithStyle?.restaurants)
+  }
   const handleClick = () =>{
     const filteredRestaurants = topRestaurants.filter((restaurant)=>{
       return restaurant.info.avgRating >= 4
@@ -20,8 +30,8 @@ const Body =() =>{
             return(
               <RestaurantCard RestaurantData={restaurant.info} key={restaurant.info.id}/>
             )
-  })
-  }
+            })
+          }
         </div>
       </div>
     )
