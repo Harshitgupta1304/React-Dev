7052ffd2fd02a887569aad661c236c63f1d16bf5
Harshitgupta1304 <guptaharshit1304@gmail.com>
2024-05-12 23:41:21 +0530

    add filtered restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import Data from './../utils/data.json'
+import { useState } from "react";
 const Body =() =>{
-
-    return(
+  const [topRestaurants, setTopRestaurants] = useState(Data?.data?.restaurants)
+  console.log(topRestaurants)
+  const handleClick = () =>{
+    const filteredRestaurants = topRestaurants.filter((restaurant)=>{
+      return restaurant.info.avgRating >= 4
+    })
+    setTopRestaurants(filteredRestaurants)
+  };
+  return(
       <div className='container'>
-        <div className='search-bar'>Search</div>
+        <div className='search-bar'>
+          <button onClick={handleClick} >Top Rated Restaurants</button>
+        </div>
         <div className='card-container'>
-          {Data.data.restaurants.map((restaurant)=>{
+          {topRestaurants.map((restaurant)=>{
             return(
               <RestaurantCard RestaurantData={restaurant.info} key={restaurant.info.id}/>
             )
